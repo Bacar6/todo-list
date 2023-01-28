d5a2c138c4f9b191e4ff53f93a039701a27272ab
gala <andhoumani13003.g@gmail.com>
2023-01-29 00:32:59 +0100

    suite to-do

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 "use strict";
 
+// CODE SUR LES AJOUTS TODO LIST
+/*
 const form = document.querySelector("form");
-
-//const monBoutton = document.getElementById("boutton");
 
 form.addEventListener("submit", (e) =>{
   e.preventDefault()
@@ -15,21 +15,41 @@ form.addEventListener("submit", (e) =>{
 
   champ.value = "";
 
-  noTache.style.display = "none"
+  //return false;
+
+});
+*/
+
+/*****************************FIXME   ***********************/
+
+const form = document.querySelector("form");
+let button = document.querySelector("button");
+
+form.addEventListener("submit", (e) =>{
+  e.preventDefault()
+
+  const li = document.createElement("li");
+
+  li.textContent = champ.value;
+
+  ol.appendChild(li)
+
+  champ.value = "";
 
   //return false;
 
 });
 
+button.addEventListener("click", () =>{
+  ol.removeChild(ol.firstElementChild)
+});
 
 
 
-
-
-
-
-
-
+function myFunction() {
+  const list = document.getElementById("myList");
+  list.removeChild(list.firstElementChild);
+}
 
 
 
