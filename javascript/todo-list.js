cbe400503d0ed0833c13fd82cbed2ba3836051ab
gala <andhoumani13003.g@gmail.com>
2023-02-13 17:27:05 +0100

    suite to-do comment sup

---
+++ javascript/todo-list.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const form = document.querySelector('form');
+const button = document.querySelector('button');
+
+form.addEventListener("submit", (e) =>{
+  e.preventDefault();
+
+  let li = document.createElement("li");
+
+  ol.appendChild(li);
+
+  li.textContent = champ.value;
+
+  champ.value = "";
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
